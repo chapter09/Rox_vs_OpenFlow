8a23d407b61337493cb7d383032bc12c9cb619fd
chapter09 <wh.sjtu@gmail.com>
2013-12-31 19:07:46 +0800

    udpate

--- flow_nums_stats.sh
+++ flow_nums_stats.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+date +%s > time.log
 while true
 do
     #ovs-ofctl dump-flows br0 | wc -l
-    ovs-appctl -t /ovs/var/run/openvswitch/ovs-vswitchd.20103.ctl pronto/dump-flows | wc -l
+    #ovs-ofctl dump-flows br0 | wc -l
+    ret=`ovs-appctl -t /ovs/var/run/openvswitch/ovs-vswitchd.$1.ctl pronto/dump-flows | wc -l`
+    echo $ret
+    if [[ $ret = 2045 ]]; then
+        ovs-ofctl dump-flows br0 | tail -n 1
+        date +%s >> time.log
     #ovs-ofctl dump-flows br0 >> flow.log
     #echo '------------------------------------------------------------------------' >> flow.log
+    fi
     sleep 0.1
 done
--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
-ovs-vsctl --db=tcp:192.168.11.1:6636 add-br br0
-ovs-vsctl --db=tcp:192.168.11.1:6636 set bridge br0 datapath_type=pronto
-ovs-vsctl --db=tcp:192.168.11.1:6636 add-port br0 ge-1/1/1 -- set Interface ge-1/1/1 type=pronto
-ovs-vsctl --db=tcp:192.168.11.1:6636 add-port br0 ge-1/1/2 -- set Interface ge-1/1/2 type=pronto
-ovs-vsctl --db=tcp:192.168.11.1:6636 set-controller br0 tcp:192.168.11.254:6633
+#!/bin/bash
+
+
+ovs-vsctl --db=tcp:$1 add-br br0
+ovs-vsctl --db=tcp:$1 set bridge br0 datapath_type=pronto
+ovs-vsctl --db=tcp:$1 add-port br0 ge-1/1/$3 -- set Interface ge-1/1/$3 type=pronto
+ovs-vsctl --db=tcp:$1 add-port br0 ge-1/1/$4 -- set Interface ge-1/1/$4 type=pronto
+ovs-vsctl --db=tcp:$1 set-controller br0 tcp:$2
 
